532e4becd41b17e1283d9a56de35d99a3be5d4cb
Sergey Samokhvalov <redirex.studio@gmail.com>
2025-08-28 16:54:30 +0300

    Changes content on the Services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { headers } from "next/headers";
-import { getDeviceInfo } from "@/app/hooks/useDevice";
-import ServiceHero from "@/app/services/service-hero/service-hero";
-import ServiceTable from "@/app/services/service-table/service-table";
+import React from 'react';
+import { headers } from 'next/headers';
+import { getDeviceInfo } from '@/app/hooks/useDevice';
+import ServiceHero from '@/app/services/service-hero/service-hero';
+import ServiceTable from '@/app/services/service-table/service-table';
 
 const Page = async () => {
   const headersList = await headers();
-  const userAgent = headersList.get("user-agent") || "";
+  const userAgent = headersList.get('user-agent') || '';
   const device = getDeviceInfo(userAgent);
 
   return (
